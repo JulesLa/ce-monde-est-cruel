93016e478b743e0b283f2ca4b1a01c97153c45ea
JulesLa <jules.lapisardi@epita.fr>
2020-10-07 20:20:20 +0200

    Fusion IA

--- src/Game/PlayerIA/OssusPlayer.php
+++ src/Game/PlayerIA/OssusPlayer.php
@@ -17,6 +17,21 @@ class OssusPlayer extends Player
 
     public function getChoice()
     {
+        //IA 133917 inverse adversaire
+        //IA 10297 Stats
+        //IA ??? inverse X premier rounds
+        if ($this->result->getNbRound() < 50) {
+            if ($this->result->getLastChoiceFor($this->mySide) === 0) {
+                return parent::rockChoice();
+            }
+            if ($this->result->getLastChoiceFor($this->opponentSide) === parent::rockChoice()) {
+                return parent::paperChoice();
+            }
+            if ($this->result->getLastChoiceFor($this->opponentSide) === parent::scissorsChoice()) {
+                return parent::rockChoice();
+            }
+            return parent::scissorsChoice();
+        }
         if ($this->result->getLastChoiceFor($this->mySide) === 0) {
             return parent::rockChoice();
         }
